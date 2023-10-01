60b1bdc42f0fa86f1a83b2bada2db70320a40366
turner-jeremy <37211811+turner-jeremy@users.noreply.github.com>
2023-10-01 11:27:25 +0800

    Create outline of QLab color picker
    Colors added as object within main js file
    Color selector works and allow color name to be copied to clipboard
    when clicked.
    Need to clean up CSS, and add options for adding syntax to clipboard.

---
+++ javascript.js
@@ -0,0 +1,677 @@
+const QLAB4_COLORS = [
+    {
+      color: "cerulean",
+      display: "Cerulean",
+      dark: "#213d43",
+      light: "#264049",
+      highlight: "#008eb6"
+    },
+    {
+      color: "green",
+      display: "Green",
+      dark: "#20482d",
+      light: "#254d32",
+      highlight: "#00d12e"
+    },
+    {
+      color: "zaffre",
+      display: "Zaffre",
+      dark: "#282d40",
+      light: "#2d3345",
+      highlight: "#0f2680"
+    },
+    {
+      color: "viridian",
+      display: "Viridian",
+      dark: "#353b39",
+      light: "#39403e",
+      highlight: "#476b5f"
+    },
+    {
+      color: "blue",
+      display: "Blue",
+      dark: "#32394b",
+      light: "#383e51",
+      highlight: "#5273e0"
+    },
+    {
+      color: "verdigris",
+      display: "Verdigris",
+      dark: "#364242",
+      light: "#3b4746",
+      highlight: "#548f8d"
+    },
+    {
+      color: "wenge",
+      display: "Wenge",
+      dark: "#373634",
+      light: "#3c3a39",
+      highlight: "#574c4b"
+    },
+    {
+      color: "avocado",
+      display: "Avocado",
+      dark: "#373d25",
+      light: "#3a422a",
+      highlight: "#689100"
+    },
+    {
+      color: "olive",
+      display: "Olive",
+      dark: "#3b3b2c",
+      light: "#414131",
+      highlight: "#6c6b20"
+    },
+    {
+      color: "indigo",
+      display: "Indigo",
+      dark: "#373742",
+      light: "#3d3b46",
+      highlight: "#6d6aaa"
+    },
+    {
+      color: "glaucous",
+      display: "Glaucous",
+      dark: "#373e44",
+      light: "#3d424a",
+      highlight: "#7092c0"
+    },
+    {
+      color: "purple",
+      display: "Purple",
+      dark: "#3d2e42",
+      light: "#413346",
+      highlight: "#74288b"
+    },
+    {
+      color: "rufous",
+      display: "Rufous",
+      dark: "#3f2e2a",
+      light: "#44332f",
+      highlight: "#802b14"
+    },
+    {
+      color: "taupe",
+      display: "Taupe",
+      dark: "#423d39",
+      light: "#46433e",
+      highlight: "#89735f"
+    },
+    {
+      color: "skyblue",
+      display: "Sky Blue",
+      dark: "#3e4549",
+      light: "#444b4e",
+      highlight: "#8eb9cc"
+    },
+    {
+      color: "sage",
+      display: "Sage",
+      dark: "#43433e",
+      light: "#484843",
+      highlight: "#969278"
+    },
+    {
+      color: "puce",
+      display: "Puce",
+      dark: "#443d3f",
+      light: "#4a4344",
+      highlight: "#99757e"
+    },
+    {
+      color: "seaFoamGreen",
+      display: "Seafoam Green",
+      dark: "#454845",
+      light: "#4a4d4a",
+      highlight: "#9bab98"
+    },
+    {
+      color: "gray",
+      display: "Grey",
+      dark: "#404042",
+      light: "#444347",
+      highlight: "#a09daa"
+    },
+    {
+      color: "vermilion",
+      display: "Vermilion",
+      dark: "#473431",
+      light: "#4c3a36",
+      highlight: "#a54b39"
+    },
+    {
+      color: "scarlet",
+      display: "Scarlet",
+      dark: "#493331",
+      light: "#4e3636",
+      highlight: "#aa3b38"
+    },
+    {
+      color: "red",
+      display: "Red",
+      dark: "#4a3330",
+      light: "#4e3835",
+      highlight: "#ae4237"
+    },
+    {
+      color: "orange",
+      display: "Orange",
+      dark: "#4b3e2f",
+      light: "#504433",
+      highlight: "#b17c2a"
+    },
+    {
+      color: "celadon",
+      display: "Celadon",
+      dark: "#434b46",
+      light: "#495149",
+      highlight: "#b8e5bc"
+    },
+    {
+      color: "ochre",
+      display: "Ochre",
+      dark: "#443a2f",
+      light: "#4a4035",
+      highlight: "#cd8d40"
+    },
+    {
+      color: "ecru",
+      display: "Ecru",
+      dark: "#48443e",
+      light: "#4c4941",
+      highlight: "#cdbe93"
+    },
+    {
+      color: "lilac",
+      display: "Lilac",
+      dark: "#484248",
+      light: "#4c484c",
+      highlight: "#cfb0ce"
+    },
+    {
+      color: "mauve",
+      display: "Mauve",
+      dark: "#47434b",
+      light: "#4c4850",
+      highlight: "#e0c3fb"
+    },
+    {
+      color: "edgecombGray",
+      display: "Edgecomb Gray",
+      dark: "#4c4a49",
+      light: "#52504d",
+      highlight: "#e2dccf"
+    },
+    {
+      color: "chartreuse",
+      display: "Chartreuse",
+      dark: "#4c5122",
+      light: "#515528",
+      highlight: "#e4fa00"
+    },
+    {
+      color: "fulvous",
+      display: "Fulvous",
+      dark: "#4e3e25",
+      light: "#52432a",
+      highlight: "#eb9700"
+    },
+    {
+      color: "maize",
+      display: "Maize",
+      dark: "#514d37",
+      light: "#54533d",
+      highlight: "#f8ea6e"
+    },
+    {
+      color: "coral",
+      display: "Coral",
+      dark: "#523e36",
+      light: "#56413a",
+      highlight: "#fe9161"
+    },
+    {
+      color: "fuchsia",
+      display: "Fuchsia",
+      dark: "#523652",
+      light: "#563a56",
+      highlight: "#ff66ff"
+    },
+    {
+      color: "bastardAmber",
+      display: "Bastard Amber",
+      dark: "#524946",
+      light: "#564e4a",
+      highlight: "#ffd2c1"
+    },
+    {
+      color: "bisque",
+      display: "Bisque",
+      dark: "#524d48",
+      light: "#56514d",
+      highlight: "#ffeace"
+    }
+  ];
+
+const QLAB5_COLORS = [
+    {
+      color: "lucasBlue",
+      display: "Lucas Blue",
+      dark: "#272c32",
+      light: "#2d3338",
+      highlight: "#0f2c52"
+    },
+    {
+      color: "midnight",
+      display: "Midnight",
+      dark: "#292938",
+      light: "#30303e",
+      highlight: "#161966"
+    },
+    {
+      color: "forest",
+      display: "Forest",
+      dark: "#2b312a",
+      light: "#323830",
+      highlight: "#22461e"
+    },
+    {
+      color: "plum",
+      display: "Plum",
+      dark: "#2c252d",
+      light: "#332c35",
+      highlight: "#35053d"
+    },
+    {
+      color: "cerulean",
+      display: "Cerulean",
+      dark: "#2d373e",
+      light: "#333d43",
+      highlight: "#3579a3"
+    },
+    {
+      color: "chocolate",
+      display: "Chocolate",
+      dark: "#312d2b",
+      light: "#373432",
+      highlight: "#422a20"
+    },
+    {
+      color: "viridian",
+      display: "Viridian",
+      dark: "#303836",
+      light: "#373e3c",
+      highlight: "#456a5b"
+    },
+    {
+      color: "blue",
+      display: "Blue",
+      dark: "#303344",
+      light: "#36394b",
+      highlight: "#465ad1"
+    },
+    {
+      color: "chestnut",
+      display: "Chestnut",
+      dark: "#322b28",
+      light: "#38312e",
+      highlight: "#4d240f"
+    },
+    {
+      color: "wenge",
+      display: "Wenge",
+      dark: "#333232",
+      light: "#3a3837",
+      highlight: "#534947"
+    },
+    {
+      color: "verdigris",
+      display: "Verdigris",
+      dark: "#333f3e",
+      light: "#3b4646",
+      highlight: "#549592"
+    },
+    {
+      color: "indigo",
+      display: "Indigo",
+      dark: "#32313c",
+      light: "#393841",
+      highlight: "#585495"
+    },
+    {
+      color: "cyan",
+      display: "Cyan",
+      dark: "#354746",
+      light: "#3b4d4e",
+      highlight: "#59bbbc"
+    },
+    {
+      color: "hotPink",
+      display: "Hot Pink",
+      dark: "#324330",
+      light: "#384a36",
+      highlight: "#5cc844"
+    },
+    {
+      color: "avocado",
+      display: "Avocado",
+      dark: "#33382a",
+      light: "#3a3e31",
+      highlight: "#5f8125"
+    },
+    {
+      color: "berry",
+      display: "Berry",
+      dark: "#342a2f",
+      light: "#3a3136",
+      highlight: "#64204b"
+    },
+    {
+      color: "glaucous",
+      display: "Glaucous",
+      dark: "#34383f",
+      light: "#3a3e46",
+      highlight: "#647eae"
+    },
+    {
+      color: "olive",
+      display: "Olive",
+      dark: "#38382b",
+      light: "#3f3f31",
+      highlight: "#6b6b20"
+    },
+    {
+      color: "violet",
+      display: "Violet",
+      dark: "#39293e",
+      light: "#3f3045",
+      highlight: "#711590"
+    },
+    {
+      color: "periwinkle",
+      display: "Periwinkle",
+      dark: "#37354a",
+      light: "#3d3b4f",
+      highlight: "#776bf6"
+    },
+    {
+      color: "skyBlue",
+      display: "Sky Blue",
+      dark: "#3c4248",
+      light: "#42484e",
+      highlight: "#7da3c1"
+    },
+    {
+      color: "crimson",
+      display: "Crimson",
+      dark: "#3b292b",
+      light: "#413032",
+      highlight: "#7e1821"
+    },
+    {
+      color: "purple",
+      display: "Purple",
+      dark: "#3a283f",
+      light: "#403045",
+      highlight: "#8819ac"
+    },
+    {
+      color: "green",
+      display: "Green",
+      dark: "#3a393c",
+      light: "#404042",
+      highlight: "#8e8a98"
+    },
+    {
+      color: "taupe",
+      display: "Taupe",
+      dark: "#3e3a35",
+      light: "#45403d",
+      highlight: "#91755e"
+    },
+    {
+      color: "maize",
+      display: "Maize",
+      dark: "#402b38",
+      light: "#46313e",
+      highlight: "#982068"
+    },
+    {
+      color: "rufous",
+      display: "Rufous",
+      dark: "#3c2a28",
+      light: "#423230",
+      highlight: "#9a2b19"
+    },
+    {
+      color: "ochre",
+      display: "Ochre",
+      dark: "#41372d",
+      light: "#483e33",
+      highlight: "#9e652d"
+    },
+    {
+      color: "ecru",
+      display: "Ecru",
+      dark: "#423f39",
+      light: "#494640",
+      highlight: "#9f9470"
+    },
+    {
+      color: "seafoamGreen",
+      display: "Seafoam Green",
+      dark: "#434742",
+      light: "#4a4d48",
+      highlight: "#a7bba1"
+    },
+    {
+      color: "yellow",
+      display: "Yellow",
+      dark: "#4a4731",
+      light: "#504e37",
+      highlight: "#afa539"
+    },
+    {
+      color: "edgecombGray",
+      display: "Edgecomb Gray",
+      dark: "#464543",
+      light: "#4c4a49",
+      highlight: "#b2aca2"
+    },
+    {
+      color: "vermilion",
+      display: "Vermilion",
+      dark: "#45312f",
+      light: "#4a3734",
+      highlight: "#b54536"
+    },
+    {
+      color: "mauve",
+      display: "Mauve",
+      dark: "#45404b",
+      light: "#4c4651",
+      highlight: "#b595d1"
+    },
+    {
+      color: "celadon",
+      display: "Celadon",
+      dark: "#3f463f",
+      light: "#474c45",
+      highlight: "#b6e0b3"
+    },
+    {
+      color: "sage",
+      display: "Sage",
+      dark: "#42403a",
+      light: "#474740",
+      highlight: "#bbb98f"
+    },
+    {
+      color: "lilac",
+      display: "Lilac",
+      dark: "#413c42",
+      light: "#484349",
+      highlight: "#be9fc1"
+    },
+    {
+      color: "puce",
+      display: "Puce",
+      dark: "#423a3c",
+      light: "#494042",
+      highlight: "#c28b99"
+    },
+    {
+      color: "fuchsia",
+      display: "Fuchsia",
+      dark: "#48314a",
+      light: "#4e3751",
+      highlight: "#c946d4"
+    },
+    {
+      color: "marigold",
+      display: "Marigold",
+      dark: "#493e2e",
+      light: "#504435",
+      highlight: "#cb8932"
+    },
+    {
+      color: "coral",
+      display: "Coral",
+      dark: "#483932",
+      light: "#503f38",
+      highlight: "#cc734d"
+    },
+    {
+      color: "orange",
+      display: "Orange",
+      dark: "#483b2d",
+      light: "#504334",
+      highlight: "#cc832f"
+    },
+    {
+      color: "lavender",
+      display: "Lavender",
+      dark: "#443b47",
+      light: "#4a414d",
+      highlight: "#cc91df"
+    },
+    {
+      color: "magenta",
+      display: "Magenta",
+      dark: "#494835",
+      light: "#504e3d",
+      highlight: "#cdc360"
+    },
+    {
+      color: "scarlet",
+      display: "Scarlet",
+      dark: "#482e2f",
+      light: "#4e3434",
+      highlight: "#d53a3b"
+    },
+    {
+      color: "fulvous",
+      display: "Fulvous",
+      dark: "#45392c",
+      light: "#4c3f32",
+      highlight: "#d78a31"
+    },
+    {
+      color: "chartreuse",
+      display: "Chartreuse",
+      dark: "#474b31",
+      light: "#4e5137",
+      highlight: "#e6fd52"
+    },
+    {
+      color: "red",
+      display: "Red",
+      dark: "#48302f",
+      light: "#4e3636",
+      highlight: "#e84944"
+    },
+    {
+      color: "gray",
+      display: "Gray",
+      dark: "#482d38",
+      light: "#4e323f",
+      highlight: "#ea337f"
+    },
+    {
+      color: "peach",
+      display: "Peach",
+      dark: "#493932",
+      light: "#4e4039",
+      highlight: "#ef895e"
+    },
+    {
+      color: "bastardAmber",
+      display: "Bastard Amber",
+      dark: "#4a4340",
+      light: "#504a47",
+      highlight: "#f6cab8"
+    },
+    {
+      color: "bisque",
+      display: "Bisque",
+      dark: "#4a4744",
+      light: "#514e4a",
+      highlight: "#fbe6c9"
+    }
+  ];
+
+  let selectedVersion = "";
+  const versionSelector = document.querySelector('.version-select');
+  const qlabVersions = versionSelector.querySelectorAll('img');
+  const colorContainer = document.querySelector('.color-container');
+  let allColors;
+  let selectedColor;
+ 
+  
+  qlabVersions.forEach((img) => {
+    img.addEventListener('click', () => {
+        qlabVersions.forEach((img) => {
+            img.classList.remove('selected')
+        });
+        img.classList.add('selected');
+        let selectedVersion = img.id;
+        displayColors(selectedVersion);
+        });
+    });
+
+
+
+function displayColors(version) {
+  version == "qlab4" ? selectedVersion= QLAB4_COLORS : selectedVersion = QLAB5_COLORS;
+  colorContainer.innerHTML = "<div></div>";
+
+  selectedVersion.forEach((color) => {
+      colorContainer.innerHTML += `
+      <div class="color" style="border-color: ${color.highlight}" id="${color.color}">
+      <div class="dark" style="background-color: ${color.dark};">${color.display}</div>
+      <div class="light" style="background-color: ${color.light};"></div>
+  </div>
+  `
+  });
+
+  colorEvents();
+};
+
+function colorEvents() {
+  allColors = document.querySelectorAll('.color');
+
+  allColors.forEach((color) => {
+    color.addEventListener('click', () => {
+      selectedColor = color.id;
+      navigator.clipboard.writeText(selectedColor);
+    })
+  });
+
+  allColors.forEach((color) => {
+    color.addEventListener('mouseover', () => {
+      selectedColor = color.id;
+      color.classList.add('.color-hover')
+    })
+  });
+
+}
